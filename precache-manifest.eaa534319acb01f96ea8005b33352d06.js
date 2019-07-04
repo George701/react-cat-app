05155c3cd94a19629e7763d5d325d96dbaa26acb
George701 <chernikov.georgii@gmail.com>
2019-07-04 15:54:18 +0300

    Updates

---
+++ precache-manifest.eaa534319acb01f96ea8005b33352d06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088fe486abc80de16608435577d69544",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cbec9cfce6a4fb5e0e1",
+    "url": "/static/css/main.55156491.chunk.css"
+  },
+  {
+    "revision": "b88582f28fd5cc4d8d7b",
+    "url": "/static/js/2.973ffd83.chunk.js"
+  },
+  {
+    "revision": "8cbec9cfce6a4fb5e0e1",
+    "url": "/static/js/main.76804f9f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dbcca15c4c9054e286a16c647dc53e41",
+    "url": "/static/media/big.dbcca15c.gif"
+  },
+  {
+    "revision": "9c95f9afbb4e69ee423b2d62d90e8d9f",
+    "url": "/static/media/small.9c95f9af.gif"
+  }
+]);
